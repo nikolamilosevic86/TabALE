33c425e0929447b5fc310c4d517baf1e1096b791
nikola.milosevic86@gmail.com <nikola.milosevic86@gmail.com>
2015-09-10 15:22:59 +0100

    Author class implemented

--- src/ScientificDocumentClasses/Author.java
+++ src/ScientificDocumentClasses/Author.java
@@ -1,5 +1,44 @@
 package ScientificDocumentClasses;
 
 public class Author {
+	private String AuthorName;
+	private String AuthorAffiliation;
+	private String AuthorEmail;
+	/**
+	 * @return the authorName
+	 */
+	public String getAuthorName() {
+		return AuthorName;
+	}
+	/**
+	 * @param authorName the authorName to set
+	 */
+	public void setAuthorName(String authorName) {
+		AuthorName = authorName;
+	}
+	/**
+	 * @return the authorAffiliation
+	 */
+	public String getAuthorAffiliation() {
+		return AuthorAffiliation;
+	}
+	/**
+	 * @param authorAffiliation the authorAffiliation to set
+	 */
+	public void setAuthorAffiliation(String authorAffiliation) {
+		AuthorAffiliation = authorAffiliation;
+	}
+	/**
+	 * @return the authorEmail
+	 */
+	public String getAuthorEmail() {
+		return AuthorEmail;
+	}
+	/**
+	 * @param authorEmail the authorEmail to set
+	 */
+	public void setAuthorEmail(String authorEmail) {
+		AuthorEmail = authorEmail;
+	}
 
 }
