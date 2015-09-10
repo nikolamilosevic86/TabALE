cde37bb954227fc571235729aaa3059eac9a5756
nikola.milosevic86@gmail.com <nikola.milosevic86@gmail.com>
2015-09-10 13:38:37 +0100

    Initial classes

---
+++ src/AnnotationReader/AnnotationReader.java
@@ -0,0 +1,5 @@
+package AnnotationReader;
+
+public class AnnotationReader {
+
+}
---
+++ src/Main/TabALERunner.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package Main;
+
+/**
+ * @author mbaxkhm4
+ *
+ */
+public class TabALERunner {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("TabALE says hi and I love you");
+
+	}
+
+}
